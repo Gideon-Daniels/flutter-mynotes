6a54ee65a5890b2a91154d797998eed8baf32ef7
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2022-03-29 11:54:58 +0200

    Protecting NotesService with Current User

---
+++ lib/extensions/list/filter.dart
@@ -0,0 +1,5 @@
+// we are extending any T that has a value of T
+extension Filter<T> on Stream<List<T>> {
+  Stream<List<T>> filter(bool Function(T) where) =>
+      map((items) => items.where(where).toList());
+}
